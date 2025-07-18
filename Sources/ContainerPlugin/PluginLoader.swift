2e1bee1bda3fdf36c82ab9ae00863627e55a4a66
J Logan <sgtbakerrahulnet@yahoo.com>
2025-07-17 22:10:03 -0700

    Fix subcommand groups in top level help. (#351)
    - Closes #349.

--- Sources/ContainerPlugin/PluginLoader.swift
+++ Sources/ContainerPlugin/PluginLoader.swift
@@ -60,7 +60,6 @@ extension PluginLoader {
         }
 
         var lines = original.split(separator: "\n").map { String($0) }
-        let footer = String(lines.removeLast())
 
         let sectionHeader = "PLUGINS:"
         lines.append(sectionHeader)
@@ -69,8 +68,6 @@ extension PluginLoader {
             let helpText = plugin.helpText(padding: 24)
             lines.append(helpText)
         }
-        lines.append("")
-        lines.append(footer)
 
         return lines.joined(separator: "\n")
     }
